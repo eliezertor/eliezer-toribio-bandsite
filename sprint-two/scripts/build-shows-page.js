7533946d2b2db6126b25094ff4b263436fcdae0a
Eliezer Toribio <eliezertor@gmail.com>
2020-09-04 23:39:35 -0400

    Javascript v1

--- sprint-two/scripts/build-shows-page.js
+++ sprint-two/scripts/build-shows-page.js
@@ -1,58 +1,100 @@
 const comments = [
   {
-    name: "Micheal Lyons",
+    name: "Theodore Duncan",
     comment:
-      "They Blew the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
+      "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
+    date: "11/15/2018",
   },
   {
     name: "Gary Wong",
     comment:
       "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself!",
+    date: "12/12/2018",
   },
   {
-    name: "Theodore Duncan",
+    name: "Micheal Lyons",
     comment:
-      "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
+      "They Blew the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
+    date: "12/18/2018",
   },
 ];
 
-// comments.map((obj, i) => {
-//   console.log("name", obj.name);
-//   console.log("comment", obj.comment);
-// });
+// MAKES DATE
+let currentDate = new Date();
+let formattedDate =
+  currentDate.getMonth() +
+  1 +
+  "/" +
+  currentDate.getDay() +
+  "/" +
+  currentDate.getFullYear();
 
-const addName = comments.map((obj, i) => {
-  let name = ("name", obj.name);
-  //   let comment = ("comment", obj.comment);
-  return name;
-});
+const form = document.getElementById("form");
 
-const addComment = comments.map((obj, i) => {
-  let comment = ("comment", obj.comment);
-  return comment;
-});
-
-// tried turning my object into a bring, but it was all concat into one string
-// let postName = addName.toString();
-// let postComments = addComment.toString();
-
-// This returns all comments as an array
-
-let postName = () => {
-  for (let i = 0; i < addName.length; i++) {
-    console.log(addName[i]);
-  }
+// TAKES VALUE FROM FORM AND PUSHES INTO COMMENTS ARRAY
+form.onsubmit = (event) => {
+  event.preventDefault();
+  const name = event.target.name.value;
+  const comment = event.target.comment.value;
+  const newComment = {
+    name: name,
+    comment: comment,
+    date: formattedDate,
+  };
+  comments.push(newComment);
+  document.querySelector(".comments__name").value = "";
+  document.querySelector(".comments__comment").value = "";
+  loadComments();
 };
 
-// This returns all comments as an array
-let postComments = () => {
-  for (let i = 0; i < addComment.length; i++) {
-    // console.log(addComment[i]);
-    let pushComment = addComment;
-    return pushComment;
-  }
-};
+// MAKES PARENT CONTAINER AND LOADS COMMENTS
+function makeParentDiv() {
+  const commentsReturn = document.createElement("div");
+  commentsReturn.classList.add("comments__return");
+  let referenceNode = document.querySelector(".comments__form");
+  referenceNode.parentNode.insertBefore(
+    commentsReturn,
+    referenceNode.nextElementSibling
+  );
+  loadComments();
+}
 
-// for (let items of postComments()) {
-//   console.log(items);
-// }
+// INVOKING
+makeParentDiv();
+
+// LOOPS OVER COMMENTS ARRAY AND CLEARS COMMENT SECTION
+function loadComments() {
+  document.querySelector(".comments__return").innerHTML = "";
+  comments
+    .slice()
+    .reverse()
+    .forEach((element) => makeSection(element));
+}
+
+// MAKES CHILD ELEMENTS AND APPENDS TO PARENT CONTAINER
+function makeSection(comment) {
+  let referenceParent = document.querySelector(".comments__return");
+  const separation = document.createElement("div");
+  separation.classList.add("comments__separation");
+  referenceParent.appendChild(separation);
+  //this makes the picture
+  const commentsPicture = document.createElement("div");
+  commentsPicture.classList.add("comments__picture");
+  separation.appendChild(commentsPicture);
+  //this makes the h2
+  let commenterName = document.createElement("h2");
+  commenterName.classList.add("comments__commenter-name");
+  separation.appendChild(commenterName);
+  // this makes the date
+  let date = document.createElement("span");
+  date.classList.add("comments__date");
+  separation.appendChild(date);
+  // this makes the paragraph
+  let commenterPara = document.createElement("p");
+  commenterPara.classList.add("comments__commenter-paragraph");
+  separation.appendChild(commenterPara);
+
+  commenterPara.innerText = comment.comment;
+  commenterName.innerText = comment.name;
+  date.innerText = comment.date;
+}
